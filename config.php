7ea56dd1b7691b1d769944ef4e879c35398cfc18
damy-ray <105303425+damy-ray@users.noreply.github.com>
2024-06-10 15:23:52 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// File di configurazione dei parametri del database
+
+$config = [
+    'host' => 'localhost',
+    'dbname' => 'travelapp',
+    'username' => 'root',
+    'password' => ''
+];
+
+?>
